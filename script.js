2eceb1c108dd0e36da102ca7778db8968d3420cc
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-04-20 09:06:04 -0300

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const loadAllItems = () => {
     axios.get('https://dummyjson.com/products')
-        .then(function (response) {
-            // handle success
+        .then(response => {
             console.log(response);
+            const itemList = document.getElementById("catalog");
+
+            response.data.products.forEach(item => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+                card.setAttribute("style", "width: 18rem;");
+                card.innerHTML = `<img src="${item.images[4]}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>`;
+
+                itemList.appendChild(card);
+            });
         })
         .catch(function (error) {
             // handle error
