e1ceb9a4efdfb7a5206507c558c277abbd9a9f9f
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-04-20 08:35:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const loadAllItems = () => {
+    axios.get('https://dummyjson.com/products')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+}
+
+loadAllItems();
