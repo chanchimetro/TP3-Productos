12d6acb506e0304e90cbc5f9fa8637c701499333
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-04-27 09:06:13 -0300

    finished listing & pagination WIP

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let itemSkip = 0;
+
 const getCategories = () => {
     axios.get('https://dummyjson.com/products/categories')
         .then(response => {
@@ -15,8 +17,8 @@ const getItemInfo = () => {
     itemId = searchParams.get('id');
     axios.get(`https://dummyjson.com/products/${itemId}`)
         .then(response => {
-            console.log(response);
-            renderCatalog(response.data.products);
+            console.log(response.data);
+            renderListing(response.data);
         })
         .catch(function (error) {
             console.log(error);
@@ -24,7 +26,11 @@ const getItemInfo = () => {
 }
 
 const loadAllItems = () => {
-    axios.get('https://dummyjson.com/products')
+    axios.get('https://dummyjson.com/products', {
+        params: {
+            skip: itemSkip
+        }
+    })
         .then(response => {
             console.log(response);
             renderCatalog(response.data.products);
@@ -66,7 +72,9 @@ const getByCategory = (cat) => {
 
 const renderCatalog = (content) => {
     const catalog = document.getElementById("catalog");
+    const productQ = document.getElementById("productQ");
     catalog.innerHTML = "";
+    productQ.innerHTML = content.length;
     content.forEach(item => {
         const card = document.createElement("div");
         card.classList.add("col-md-2");
@@ -93,4 +101,25 @@ const renderCategories = (cats) => {
         catLi.innerHTML = `<a class="dropdown-item" onclick="getByCategory('${cat}')">${cat}</a>`
         catList.appendChild(catLi);
     });
+}
+
+const renderListing = (product) => {
+    const carousel = document.getElementById("carouselInner");
+    const brandCat = document.getElementById("brandCat");
+    const title = document.getElementById("productTitle");
+    const desc = document.getElementById("productDesc");
+    const price = document.getElementById("productPrice");
+
+    product.images.forEach(img => {
+        console.log(img);
+        const imgCont = document.createElement("div");
+        imgCont.classList.add("carousel-item");
+        imgCont.classList.add("active");
+        imgCont.innerHTML = `<img src="${img}" class="d-block w-100 rounded" alt="No image available">`;
+        carousel.appendChild(imgCont);
+    });
+    brandCat.innerHTML = `${product.category} - ${product.brand}`;
+    title.innerHTML = `${product.title}`;
+    desc.innerHTML = `${product.description}`;
+    price.innerHTML = `$ ${product.price}`;
 }
